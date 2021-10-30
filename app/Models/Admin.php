45c44c6d0de2f21a1f4a2693991bcbc27b28246f
ymahadew <ymahadew@qualogy.com>
2021-10-30 11:38:03 -0300

    login

---
+++ app/Models/Admin.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Foundation\Auth\User as Authenticatable;
+use Illuminate\Notifications\Notifiable;
+
+class Admin extends Authenticatable
+{
+    use HasFactory, Notifiable;
+
+    protected $fillable = [
+        'name',
+        'email',
+        'password',
+        'role',
+    ];
+
+    protected $hidden = [
+        'password',
+        'remember_token',
+    ];
+
+
+    public function up()
+{
+    Schema::create('admins', function (Blueprint $table) {
+        $table->id();
+        $table->string('name');
+        $table->string('email')->unique();
+        $table->enum('role',['admin','editor','operator'])->default('operator');
+        $table->string('password');
+        $table->rememberToken();
+        $table->timestamps();
+    });
+}
+}
