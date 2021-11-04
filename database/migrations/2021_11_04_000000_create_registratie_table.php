eead1921dc27eecdf9ecc7369b3c4153315460d9
shivanb09 <shivanb09@outlook.com>
2021-11-04 14:46:51 -0300

    registratie migration

---
+++ database/migrations/2021_11_04_000000_create_registratie_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateAdminsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('registratie', function (Blueprint $table) {
+            $table->id();
+            $table->string('firstname');
+            $table->string('lastname');
+            $table->string('phonenumber');
+            $table->string('adress');
+            $table->string('id_number');
+            $table->string('opmerking');
+            $table->integer('location');
+            $table->string('status');
+            $table->string('email');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('registratie');
+    }
+}
--- resources/views/adminregistratie/edit.blade.php
+++ resources/views/adminregistratie/edit.blade.php
@@ -9,68 +9,73 @@
         </div>
     </div>
 </div>
-
-@if ($errors->any())
-    <div class="alert alert-danger">
-        <strong>Whoops!</strong> There were some problems with your input.<br><br>
-        <ul>
-            @foreach ($errors->all() as $error)
-                <li>{{ $error }}</li>
-            @endforeach
-        </ul>
-    </div>
-@endif
+<table class="table table-bordered">
+         <tr>
+             <th>ID-nummer</th>
+             <td>FS230000</td>
+             <th>Registratie datum</th>
+             <td>5-11-2021</td>
+        </tr>
+        <tr>
+             <th>Naam</th>
+             <td>Shivan Bhagwandin</td>
+             <th>Adres</th>
+             <td>Lelydorp</td>
+        </tr>
+        <tr>
+             <th>Geboorte datum</th>
+             <td>09-19-2000</td>
+             <th >Telefoon</th>
+             <td>8920264</td>
+        </tr>
+     </table>
 
 <form action="{{ route('preregister') }}" method="POST">
     @csrf
     <div class="form-group row">
-       <label for="" class="col-sm-2 col-form-label">Voornaam<r style="color:red;">*</r></label>
-         <div class="col-sm-10">
+       <label for="" class="col-sm-2 col-form-label">Saturatie</label>
+         <div class="col-sm-4">
             <input type="text" name ="firstname" class="form-control" id="" placeholder="">
-         </div>
+        </div>
     </div>
     <div class="form-group row">
-       <label for="" class="col-sm-2 col-form-label">Familienaam<r style="color:red;">*</r></label>
-         <div class="col-sm-10">
+    <label for="" class="col-sm-2 col-form-label">Bloeddruk</label>
+         <div class="col-sm-2">
             <input type="text" name="lastname" class="form-control" id="" placeholder="">
          </div>
-    </div>
+         <div class="col-sm-2">
+            <input type="text" name="lastname" class="form-control" id="" placeholder="">
+         </div>
+</div>
     <div class="form-group row">
-         <label for="" class="col-sm-2 col-form-label">ID-Nummer<r style="color:red;">*</r></label>
-         <div class="col-sm-10">
-           <input type="text" name="id_number" class="form-control" id="" placeholder="">
+         <label for="" class="col-sm-2 col-form-label">Gevaccineerd</label>
+         <div class="col-sm-4">
+         <select id="" class='form-control' name= "location"  onchange="yesnoCheck(this);">
+        <option value="">Selecteer status</option>
+        <option value="wel">Wel</option>
+        <option value="niet">Niet</option>
+      </select>
+         </div>
+         </div>
+
+         <div class="form-group row" id="ifYes" style="display: none;">
+         <label for="" class="col-sm-2 col-form-label">Dosis</label>
+         <div class="col-sm-4">
+         <select id="" class='form-control' name= "location" >
+        <option value="">Selecteer status</option>
+        <option value="wel">1</option>
+        <option value="niet">2</option>
+        <option value="wel">3</option>
+      </select>
          </div>
        </div>
-       <div class="form-group row">
-         <label for="" class="col-sm-2 col-form-label">Adres<r style="color:red;">*</r></label>
-         <div class="col-sm-10">
-           <input type="text" name="adress" class="form-control" id="" placeholder="">
-         </div>
-       </div>
-     <div class="form-group row">
-         <label for="" class="col-sm-2 col-form-label">Telefoon<r style="color:red;">*</r></label>
-         <div class="col-sm-10">
-           <input type="text" name="phonenumber" class="form-control" id="" placeholder="">
-         </div>
-       </div>
-       <div class="form-group row">
-         <label for="" class="col-sm-2 col-form-label">E-mail</label>
-         <div class="col-sm-10">
-           <input type="email" name="email" class="form-control" id="" placeholder="">
-         </div>
-       </div>
-       <div class="form-group row">
-         <label for="" class="col-sm-2 col-form-label">Symptomen</label>
-         <div class="col-sm-10">
-           <textarea type="text" name="symptoms" class="form-control" id="" placeholder=""></textarea>
-         </div>
-       </div>
+       
        
       
 
        </div>
         <div class="col-xs-12 col-sm-12 col-md-12 text-center mb-4">
-                <button type="submit" class="btn btn-primary">Versturen</button>
+                <button type="submit" class="btn btn-primary">Swabben</button>
                 <a class="btn btn-primary" href="#"> Terug</a>
         </div>
     </div>
